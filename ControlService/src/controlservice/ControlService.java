ea16c1f5e7b593a86935a8fa8fa07d4740ae0f33
Lubomir Hrbacek <lubomirhrbacek@seznam.cz>
2022-05-18 13:15:23 +0200

    Refactor ControlService.java

--- ControlService/src/controlservice/ControlService.java
+++ ControlService/src/controlservice/ControlService.java
@@ -30,12 +30,15 @@ public class ControlService {
 
     
     // section with atrs
-        //73696d706c666170706c6575
 
-    private static String LOGAPPLET_AID = "73696D706C666170706C6575";
-    private static byte LOGAPPLET_AID_BYTE[] = Util.hexStringToByteArray(LOGAPPLET_AID);
+    private static String REPLAY_CARD_ATR1 = "";
+    private static String REPLAY_CARD_ATR2 = "";
+    private static String TARGET_CARD_ATR1 = "";
+    private static String TARGET_CARD_ATR2 = "";
 
-   
+    private static String REPLAY_CARD_ATR_ARR[] = { REPLAY_CARD_ATR1, REPLAY_CARD_ATR2 };
+    private static String TARGET_CARD_ATR_ARR[] = { TARGET_CARD_ATR1, TARGET_CARD_ATR2 };
+  
     private byte[] log = null;
     
     
@@ -50,13 +53,13 @@ public class ControlService {
                 main.automate();
             }
             
-            final CardManager cardMngrReplay = main.ConnectPhysicalCard(LOGAPPLET_AID_BYTE, REPLAY_CARD_ATR_ARR);
+            final CardManager cardMngrReplay = main.ConnectPhysicalCard(REPLAY_CARD_ATR_ARR, false);
             
             main.getCardSize(cardMngrReplay);
             
             if (args.length > 0 && args[0].equals("-r")) {
                 System.out.println("RESETING LOG");
-                main.ResetCardMem(cardMngrReplay);
+                main.ResetLog();
                 main.writeInReplay(cardMngrReplay);
             }
             
@@ -67,7 +70,7 @@ public class ControlService {
             main.readOutReplay(cardMngrReplay);
             main.printLog();
             
-            final CardManager cardMngrTarget = main.ConnectPhysicalCard(TARGET_APPLET_AID_BYTE, TARGET_CARD_ATR_ARR);
+            final CardManager cardMngrTarget = main.ConnectPhysicalCard(TARGET_CARD_ATR_ARR, false);
             
             main.communicateTarget(cardMngrTarget);
             main.printLog();
@@ -127,7 +130,6 @@ public class ControlService {
         // works only if error sw1 and sw2 does not have any data
         System.out.println("COMMUNICATE");
                
-        
         byte iter = 0;
         short offset = 0;
         short dataLen = 0;
@@ -151,15 +153,6 @@ public class ControlService {
             setShort(log, (short) (offset + LOG_OFFSET_DATA + rspsData.length), (short) response.getSW());
             offset = (short) ((short) (offset + LOG_OFFSET_DATA) + (short) ((short) rspsData.length + (short) 2));
             iter++;
-            
-            /*
-            offset = (short) ((short) (offset + LOG_OFFSET_DATA) + dataLen);
-            log[offset] = iter;
-            log[offset + LOG_OFFSET_TYPE] = RSPS_T;
-            setShort(log, (short) (offset + LOG_OFFSET_LEN), (short) rspsData.length);
-            java.lang.System.arraycopy(rspsData, 0, log, offset + LOG_OFFSET_DATA, rspsData.length);
-            offset = (short) ((short) (offset + LOG_OFFSET_DATA) + rspsData.length);
-            iter++;*/
         }
     }
     
@@ -168,7 +161,7 @@ public class ControlService {
         while (iter < 4) {
             CardManager cardMngrReplay;
             try {
-                cardMngrReplay = ConnectPhysicalCard(LOGAPPLET_AID_BYTE, REPLAY_CARD_ATR_ARR);
+                cardMngrReplay = ConnectPhysicalCard(REPLAY_CARD_ATR_ARR, false);
             } catch (Exception e) {
                 iter++;
                 System.out.println("Connection will be repeated in 5 seconds, insert replay card.");
@@ -179,9 +172,8 @@ public class ControlService {
             
             getCardSize(cardMngrReplay);
             readOutReplay(cardMngrReplay);
-            //printLog();
             
-            final CardManager cardMngrTarget = ConnectPhysicalCard(TARGET_APPLET_AID_BYTE, TARGET_CARD_ATR_ARR);
+            final CardManager cardMngrTarget = ConnectPhysicalCard(TARGET_CARD_ATR_ARR, false);
             
             communicateTarget(cardMngrTarget);
             printLog();
@@ -201,21 +193,20 @@ public class ControlService {
             if (ptr >= log.length) {
                 break;
             }
-            short dataLen = Util.getShort(log, (short) (ptr+(short)2));
+            short dataLen = Util.getShort(log, (short) (ptr + 2));
             if (dataLen == 0) {
                 break;
             }
             System.out.print(Util.toHex(log, ptr, 1));
-            System.out.print(Util.toHex(log, ptr+1, 1));
-            System.out.print(Util.toHex(log, ptr+2, 2));
+            System.out.print(Util.toHex(log, ptr + 1, 1));
+            System.out.print(Util.toHex(log, ptr + 2, 2));
             ptr = (short) (ptr + 4);
             for (int i = 0; i < dataLen; i++) {
                 System.out.print(Util.toHex(log, ptr+i, 1));
             }
             System.out.println("|");
             ptr = (short) (ptr + dataLen);
-        }
-        
+        }   
     }
     
     public void changeLog() {
@@ -223,15 +214,12 @@ public class ControlService {
     }
     
     private void getCardSize(CardManager cardMngr) throws Exception {
-        
         ResponseAPDU response = cardMngr.transmit(new CommandAPDU(CLA_LOGAPPLET, INS_SEND_LOG_LEN, 0x00, 0x00));
         byte[] data = response.getData();
         short CardSize = Util.getShort(data, (short) 0);
         log = new byte[CardSize];
-        //Util.arrayFillNonAtomic(log, (short) 0, (short) log.length, (byte) 0);
         java.util.Arrays.fill(log, (byte) 0);
-    }
-    
+    }  
     
     private boolean CheckATRs(Card card, String[] cardATRs) {
         for (byte i = 0; i < cardATRs.length; i++) {
@@ -242,14 +230,13 @@ public class ControlService {
         return false;
     }
     
-    
-    private CardManager ConnectPhysicalCard(byte[] APPLET_AID, String[] cardATRs) throws Exception {
+    private CardManager ConnectPhysicalCard(String[] cardATRs, boolean debug) throws Exception {
         TerminalFactory factory = TerminalFactory.getDefault();
 
         try {
             for (CardTerminal t : factory.terminals().list()) {
                 if (t.isCardPresent()) {
-                    CardManager cardMngr = new CardManager(true, APPLET_AID);
+                    CardManager cardMngr = new CardManager(debug, null);
                     System.out.println("Card found: " + t.getName());
                     System.out.print("Connecting...");
                     Card card = t.connect("*");
@@ -264,26 +251,17 @@ public class ControlService {
                     System.out.print("Establishing channel...");
                     cardMngr.setChannel(card.getBasicChannel());
                     System.out.println(" Done.");
-                    //System.out.println("Smartcard: Selecting applet...");
-                    //CommandAPDU cmd = new CommandAPDU(0x00, 0xa4, 0x04, 0x00, APPLET_AID);
-                    //ResponseAPDU response = cardMngr.transmit(cmd);
-                    //if (response.getSW() != 0x9000) {
-                    //    System.out.println("Cannot connect to applet - possibly wrong card.");
-                    //} else {
-                    //    System.out.println("Found correct card. Connected.");
                     return cardMngr;
-                    //}
                 }
             }
         } catch (Exception e) {
             System.out.println("Failed.");
             throw e;
         }
-        throw new Exception("No card with corresponding applet found.");
+        throw new Exception("No card with corresponding ATR found.");
     }
     
-    private void ResetCardMem(CardManager crdMngr) {
-        //Util.arrayFillNonAtomic(log, (short) 0, (short) log.length, (byte) 0);
+    private void ResetLog() {
         java.util.Arrays.fill(log, (byte) 0);
     }
     
